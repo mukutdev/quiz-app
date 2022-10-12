6f4c4ba75fea9b82a117e03e4df972afbcacd695
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-10-12 10:39:37 +0600

    all-blog-added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -9,39 +9,57 @@ const Blog = () => {
           tabIndex={0}
           className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-4"
         >
-              <input type="checkbox" /> 
+          <input type="checkbox" />
 
           <div className="collapse-title text-xl font-medium text-indigo-600">
-           How does context api work ?
+            How does context api work ?
           </div>
           <div className="collapse-content">
-            <p className="font-medium">The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+            <p className="font-medium">
+              The React Context API is a way for a React app to effectively
+              produce global variables that can be passed around. This is the
+              alternative to "prop drilling" or moving props from grandparent to
+              child to parent, and so on. Context is also touted as an easier,
+              lighter approach to state management using Redux.
+            </p>
           </div>
         </div>
         <div
           tabIndex={0}
           className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
         >
-              <input type="checkbox" /> 
+          <input type="checkbox" />
 
           <div className="collapse-title text-xl font-medium text-indigo-600">
-            What is the purpose of react  router ?
+            What is the purpose of react router ?
           </div>
           <div className="collapse-content">
-            <p className="font-medium">ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
+            <p className="font-medium">
+              ReactJS Router is mainly used for developing Single Page Web
+              Applications. React Router is used to define multiple routes in
+              the application. When a user types a specific URL into the
+              browser, and if this URL path matches any 'route' inside the
+              router file, the user will be redirected to that particular route.
+            </p>
           </div>
         </div>
         <div
           tabIndex={0}
-          className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+          className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-4"
         >
-              <input type="checkbox" /> 
+          <input type="checkbox" />
 
           <div className="collapse-title text-xl font-medium text-indigo-600">
-            What is the purpose of react  router ?
+            How useRef() hook works ?
           </div>
           <div className="collapse-content">
-            <p className="font-medium">ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
+            <p className="font-medium">
+              useRef returns a mutable ref object whose .current property is
+              initialized to the passed argument ( initialValue ). The returned
+              object will persist for the full lifetime of the component.
+              Essentially, useRef is like a “box” that can hold a mutable value
+              in its .current property.
+            </p>
           </div>
         </div>
       </div>
