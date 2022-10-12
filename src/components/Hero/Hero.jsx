cb6fc2edb207939e3e2bb480f23848eaeebc2eb4
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-10-12 11:29:55 +0600

    hero-responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div className="hero-wrapper">
-      <div className="container mx-auto mt-20">
-        <div className="flex items-center gap-32 mx-auto justify-center">
-          <div className="w-2/5">
-            <h1 className="text-5xl font-bold">Test your Skills</h1>
+      <div className="container mx-auto md:mt-20 mt-10">
+        <div className="flex md:flex-row  flex-col-reverse items-center md:gap-32  gap-7 md:mx-auto justify-center">
+          <div className="md:w-2/5 md:mx-0 mx-5 ">
+            <h1 className="md:text-5xl text-3xl font-bold">Test your Skills</h1>
             <p className="mt-8 text-base font-medium">
               We have created a platform for you. Lest make it happen
             </p>
@@ -14,9 +14,9 @@ const Hero = () => {
               <button className="btn btn-primary mt-8">Get Started</button>
             </Link>
           </div>
-          <div className="hero-img w-2/5">
+          <div className="hero-img md:w-2/5 md:mx-0 mx-5">
             <img
-              className="h-3/4 rounded"
+              className="h-3/4 rounded "
               src="https://placeimg.com/1000/800/arch"
               alt=""
             />
